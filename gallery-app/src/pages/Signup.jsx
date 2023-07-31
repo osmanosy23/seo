6fac5192d0b4973f0960be781aa77b35c42b5f9d
Sean Onamade <sean.d.onamade@vanderbilt.edu>
2023-07-31 11:09:06 -0600

    Signup Setup Jul 31

--- gallery-app/src/pages/Signup.jsx
+++ gallery-app/src/pages/Signup.jsx
@@ -1,8 +1,43 @@
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { galleryDescription } from './description';
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    // Prevent multiple form submissions
+    if (isSubmitting) {
+      return;
+    }
+
+    // Set the submission status to true
+    setIsSubmitting(true);
+
+    const auth = getAuth();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Handle the successful sign-up
+        setIsSubmitting(false);
+        const user = userCredential.user;
+        console.log("User signed up successfully:", user);
+      })
+      .catch((error) => {
+        // Handle sign-up errors
+        setIsSubmitting(false);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Sign-up error:", errorCode, errorMessage);
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSignUp} name="signupForm">
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col">
           <div className="text-center">
@@ -14,16 +49,30 @@ const Signup = () => {
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Signup</button>
+                <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
+                  {isSubmitting ? "Signing up..." : "Signup"}
+                </button>
               </div>
             </div>
           </div>
@@ -37,3 +86,64 @@ const Signup = () => {
 };
 
 export default Signup;
+
+// const Signup = () => {
+//   return (
+//     <form>
+//       <div className="hero min-h-screen bg-base-200">
+//         <div className="hero-content flex-col">
+//           <div className="text-center">
+//             <h1 className="text-5xl font-bold">Gallery Pro</h1>
+//           </div>
+//           <div className="card sm:w-[30rem] shadow-2xl bg-base-100">
+//             <div className="card-body">
+//               <div className="form-control">
+//                 <label className="label">
+//                   <span className="label-text">Email</span>
+//                 </label>
+//                 <input type="text" placeholder="email" className="input input-bordered" />
+//               </div>
+//               <div className="form-control">
+//                 <label className="label">
+//                   <span className="label-text">Password</span>
+//                 </label>
+//                 <input type="text" placeholder="password" className="input input-bordered" />
+//               </div>
+//               <div className="form-control mt-6">
+//                 <button className="btn btn-primary">Signup</button>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="text-center mt-6">
+//             <p>{galleryDescription}</p>
+//           </div>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// };
+
+// const handleSignUp = (event) => {
+//   event.preventDefault();
+
+//   const email = event.target.email.value;
+//   const password = event.target.password.value;
+
+//   const auth = getAuth();
+
+//   createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//       // Handle the successful sign-up, e.g., redirect to another page or show a success message.
+//       const user = userCredential.user;
+//       console.log("User signed up successfully:", user);
+//     })
+//     .catch((error) => {
+//       // Handle sign-up errors, e.g., display error message to the user.
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.error("Sign-up error:", errorCode, errorMessage);
+//     });
+// };
+
+
+// export default Signup;
