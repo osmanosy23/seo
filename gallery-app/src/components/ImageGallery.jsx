f13075395787e69f7d88766e6245d7f0e08510d8
Sean Onamade <sean.d.onamade@vanderbilt.edu>
2023-08-06 15:39:57 -0600

    Infinite scroll functionality for random images

--- gallery-app/src/components/ImageGallery.jsx
+++ gallery-app/src/components/ImageGallery.jsx
@@ -1,11 +1,48 @@
 import useFirestore from "../hooks/useFirestore";
 import getUnsplashImages from "../unsplash";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const ImageGallery = () => {
   const { docs: images, isLoading } = useFirestore('images');
   // console.log(docs)
   const [randomImage, setRandomImage] = useState(null);
+  const lastImageRef = useRef(null); // Create a ref for the last image element
+
+  useEffect(() => {
+    // Set up the Intersection Observer
+    const observer = new IntersectionObserver(
+      handleObserverCallback,
+      {
+        root: null,
+        rootMargin: "20px",
+        threshold: 1.0,
+      }
+    );
+
+    if (lastImageRef.current) {
+      observer.observe(lastImageRef.current);
+    }
+
+    return () => {
+      // Clean up the Intersection Observer when component unmounts
+      if (lastImageRef.current) {
+        observer.unobserve(lastImageRef.current);
+      }
+    };
+  }, [randomImage]);
+
+  const handleObserverCallback = async (entries) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      // When the last image comes into view, load more random images
+      try {
+        const randomImages = await getUnsplashImages();
+        setRandomImage((prevRandomImage) => [...prevRandomImage, ...randomImages]);
+      } catch (error) {
+        console.error('Error fetching random images:', error);
+      }
+    }
+  };
 
   if (isLoading) {
     return (
@@ -56,8 +93,8 @@ const ImageGallery = () => {
                 <div className="mt-8">
           <h2 className="text-4xl text-center">Random Images</h2>
           <div className="grid md:grid-cols-3 justify-center gap-4 mt-10">
-          {randomImage.map((image) => (
-            <div key={image.id} style={{ margin: '10px 0' }}>
+          {randomImage.map((image, index) => (
+            <div key={image.id} ref={index === randomImage.length - 1 ? lastImageRef : null} style={{ margin: '10px 0' }}>
               <div className="card card-compact bg-base-100 shadow-xl">
                 <figure className='max-h-[15rem]'>
               <img src={image.urls?.regular} alt="Random Image" className="random-image" style={{ maxWidth: '300px' }}/>
